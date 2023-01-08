a3653e374094d64de3f328d5bddab0d62e7ff773
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-08 10:39:25 +0300

    Create metod_ustsinirdondurme.java

---
+++ metod_ustsinirdondurme.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class metod_ustsinirdondurme {
+
+    public static int[] random_array(int ub, int N) {
+        Scanner input = new Scanner(System.in);
+
+        int[] a = new int[N];
+        Random r = new Random();
+
+        for (int i = 0; i < N; i++) {
+            a[i] = r.nextInt(ub);
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] b = random_array(65, 3);
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+    }
+
+}
