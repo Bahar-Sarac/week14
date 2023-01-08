c243bace9153519d6e6d546e9b35d7a4b82d1fb3
Bahar-Sarac <117797408+Bahar-Sarac@users.noreply.github.com>
2023-01-08 10:33:43 +0300

    Create metod_yildizucgeni.java

---
+++ metod_yildizucgeni.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class metod_yildizucgeni {
+
+    public static void exp(int row) {
+
+        for (int i = 0; i <= row; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j <= row; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Satır sayısı giriniz: ");
+        int row = input.nextInt();
+        exp(row);
+    }
+
+}
